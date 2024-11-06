111c619284b1992fba0cbc39e1a429bb82bdd190
Nikita Shub <nikitushu2@gmail.com>
2024-11-06 12:20:52 +0200

    do step 3

--- src/EmployeeCard.jsx
+++ src/EmployeeCard.jsx
@@ -5,21 +5,24 @@ export default function EmployeeCard(props) {
     const {name, role, salary, startDate, passion, ...rest} = props;
     const [job, setJob] = useState(props.role);
     const [action, setAction] = useState("Promote");
+    const [icon, setIcon] = useState("")
 
     function onPromote() {
-        if (job === "team lead") {
+        if (job === "Team Lead") {
             setJob(role);
             setAction("Promote");
+            setIcon("");
         } else {
-            setJob("team lead");
+            setJob("Team Lead");
             setAction("Demote");
+            setIcon("⭐");
         }
     }
 
     return (
     <div className="card" {...rest}>
         <p><b>Name:</b> {name}</p>
-        <p><b>Role:</b> {job}</p>
+        <p><b>Role:</b> {job} {icon}</p>
         <p><b>Salary:</b> {salary}</p>
         <p><b>Start date:</b> {startDate}</p>
         <p><b>Passion:</b> {passion}</p>
