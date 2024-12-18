d0d75ecdd279ce6c00be1d3c54e03ecbc3845a25
Nikita Shub <nikitushu2@gmail.com>
2024-12-18 12:28:02 +0200

    fix port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Use Railway's PORT or default to 5173
+    host: true, // Allow access via the assigned Railway domain
+  },
 })
